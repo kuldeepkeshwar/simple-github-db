3fccdca8b23f1fa5671fa19806bd38a8afef2266
kuldeepkeshwar <kuldeepkeshwar@gmail.com>
2019-07-19 15:05:17 +0530

    added delete/update api

--- index.js
+++ index.js
@@ -1,33 +1,103 @@
-const axios= require("axios");
+const axios = require("axios");
 
-const url= `https://github-db.glitch.me/db/api`;
+const url = `https://github-db.glitch.me/db/api`;
 
-function Client({db,token}){
-    const options={db,token};
-    let validated=false;
-    async function validate(){
-        if(!validated){
-            const {data}=await axios.get(`${url}/validate`,{params:options});
-            validated=data.valid
-        }
+function Client({ db, token }) {
+  const options = { db, token };
+  let validated = false;
+  async function validate() {
+    try {
+      if (!validated) {
+        const { data } = await axios.get(`${url}/validate`, {
+          params: options
+        });
+        validated = data.valid;
+      }
+    } catch (error) {
+      hanlderError(error);
     }
-    async function add(document,payload){
-        await validate();
-        const resp=await axios.post(`${url}/add`,{db:options.db,token,document,payload});
-        return resp.data.identifier;
+  }
+  async function add(document, payload) {
+    try {
+      await validate();
+      const resp = await axios.post(`${url}/add`, {
+        db: options.db,
+        token,
+        document,
+        payload
+      });
+      return resp.data;
+    } catch (error) {
+      hanlderError(error);
     }
-    async function fetchOne(document,identifier){
-        await validate();
-        const resp=await axios.get(`${url}/fetchOne`,{params:{db:options.db,token,document,identifier}});
-        return resp.data;
+  }
+  async function update(document, identifier, payload) {
+    try {
+      await validate();
+      const resp = await axios.post(`${url}/update`, {
+        db: options.db,
+        token,
+        document,
+        identifier,
+        payload
+      });
+      return resp.data;
+    } catch (error) {
+      hanlderError(error);
     }
-    async function fetchAll(document){
-        await validate();
-        const resp=await axios.get(`${url}/fetchAll`,{params:{db:options.db,token,document}});
-        return resp.data;
+  }
+  async function _delete(document, identifier) {
+    try {
+      await validate();
+      const resp = await axios.post(`${url}/delete`, {
+        db: options.db,
+        token,
+        document,
+        identifier
+      });
+      return resp.data;
+    } catch (error) {
+      hanlderError(error);
     }
-    return {
-        add,fetchOne,fetchAll
+  }
+  async function fetchOne(document, identifier) {
+    try {
+      await validate();
+      const resp = await axios.get(`${url}/fetchOne`, {
+        params: { db: options.db, token, document, identifier }
+      });
+      return resp.data;
+    } catch (error) {
+      hanlderError(error);
     }
+  }
+  async function fetchAll(document) {
+    try {
+      await validate();
+      const resp = await axios.get(`${url}/fetchAll`, {
+        params: { db: options.db, token, document }
+      });
+      return resp.data;
+    } catch (error) {
+      hanlderError(error);
+    }
+  }
+  return {
+    add,
+    fetchOne,
+    fetchAll,
+    delete: _delete,
+    update
+  };
 }
-module.exports= Client;
+function hanlderError(error) {
+  if (error.response) {
+    throw new Error(error.response.data);
+  } else if (error.request) {
+    throw error.request;
+  } else {
+    throw new Error(error.message);
+  }
+}
+
+module.exports = Client;
--- test.js
+++ test.js
@@ -1,19 +1,27 @@
 
-const GithubDb=require("github-db");
+const GithubDb=require("./index");
 
-const db= GithubDb({db:"test-db",token:"test"}); 
+const db= GithubDb({db:process.env.db,token:process.env.token}); 
 (async function(){
     try {
-        const id=await db.add("user",{name:"John"});
-        console.log("added user", id);
+        const {identifier:id1}=await db.add("user",{name:"John"});
+        console.log("added user", id1);
 
-        const id2=await db.add("user",{name:"Will"});
+        const {identifier:id2}=await db.add("user",{name:"Will"});
         console.log("added user", id2);
 
-        const user=await db.fetchOne("user",id);
+        const user=await db.fetchOne("user",id1);
         console.log("fetched user", user);
+
+        const updated=await db.update("user",id1,{name:"John Cena"});
+        console.log("updated user", updated);
+        
         const users=await db.fetchAll("user");
         console.log("fetched users", users);
+        for (const {identifier} of users) {
+            const result=await db.delete("user",identifier);
+            console.log("delete users", result);
+        }
     } catch (error) {
         console.error(error);
     }
